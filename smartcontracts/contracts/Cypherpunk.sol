6f737e9088fac1196cd56049ceea0d4aca39cc10
“Rahul” <rpatni@atomic.vc>
2022-07-18 19:53:02 -0700

    cypherpunk start

---
+++ smartcontracts/contracts/Cypherpunk.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Cpyherpunk is ERC721, Ownable {
+    uint256 public constant MAX_PUNKS = 10000;
+    uint256 public total = 0;
+    string private baseURI;
+    uint256 public price;
+
+    constructor(string memory baseURI_, uint256 price_)
+        ERC721("Cypherpunk", "CYPHERPUNK")
+    {
+        baseURI = baseURI_;
+        price = price_;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    function mint() internal virtual {
+        require(total < MAX_PUNKS, "Sold Out");
+        _safeMint(msg.sender, total, "");
+        total += 1;
+        refundIfOver();
+    }
+
+    function refundIfOver() private {
+        require(msg.value >= price, "Not enough ETH");
+        if (msg.value > price) {
+            payable(msg.sender).transfer(msg.value - price);
+        }
+    }
+}
