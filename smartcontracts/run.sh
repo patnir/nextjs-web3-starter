4c46dc7e0b88634fec55befb41cf10780b84b115
“Rahul” <rpatni@atomic.vc>
2022-07-18 20:27:29 -0700

    updating run.sh

--- smartcontracts/run.sh
+++ smartcontracts/run.sh
@@ -1,5 +1,5 @@
 yarn hardhat node &
 P1=$!
-yarn hardhat run --network localhost scripts/deploy.ts && cp ./artifacts/contracts/Greeter.sol/Greeter.json /home/app/contracts && cp ./*Address.json /home/app/contracts &
+yarn hardhat run --network localhost scripts/deploy.ts && cp ./artifacts/contracts/Cypherpunk.sol/Cypherpunk.json /home/app/contracts && cp ./*Address.json /home/app/contracts &
 P2=$!
 wait $P1 $P2
