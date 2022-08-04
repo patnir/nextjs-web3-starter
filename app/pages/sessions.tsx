3a7368aa09342142a996466cec4dcaa704d18d5c
Nishiki Liu <hello@nshki.com>
2022-08-03 20:50:36 -0700

    Add mint button to sessions page

--- app/pages/sessions.tsx
+++ app/pages/sessions.tsx
@@ -1,4 +1,5 @@
 import { signInWithWallet } from "lib/auth.client";
+import { mint } from "lib/chain.client";
 import type { NextPage } from "next";
 import { signOut, useSession } from "next-auth/react";
 
@@ -8,12 +9,22 @@ const ConnectedView = () => {
   return (
     <>
       <p>{`Welcome, ${session.data?.walletAddress}.`}</p>
-      <button
-        className="bg-red-600 py-2 px-4 rounded-full text-white"
-        onClick={() => signOut()}
-      >
-        {`Disconnect wallet`}
-      </button>
+
+      <div className="flex gap-4 my-4">
+        <button
+          className="bg-red-600 py-2 px-4 rounded-full text-white"
+          onClick={() => signOut()}
+        >
+          {`Disconnect wallet`}
+        </button>
+
+        <button
+          className="bg-green-600 py-2 px-4 rounded-full text-white"
+          onClick={async () => await mint()}
+        >
+          Mint
+        </button>
+      </div>
     </>
   );
 };
