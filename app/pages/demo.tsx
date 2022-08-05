2322b90108a329dea8949630b32906c439ed8bb7
Rahul Patni <rahul20patni@gmail.com>
2022-08-05 11:45:11 -0700

    working nfts

--- app/pages/demo.tsx
+++ app/pages/demo.tsx
@@ -2,12 +2,35 @@ import { signInWithWallet } from "lib/auth.client";
 import { getBalance, mint } from "lib/chain.client";
 import type { NextPage } from "next";
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const ConnectedView = () => {
   const session = useSession();
-  const [nftBalance, setNFTBalance] = useState(0)
+  const [nftBalance, setNFTBalance] = useState(-1)
+  const [nfts, setNFTs] = useState([])
 
+  const getNFTs = async () => {
+    if (session.data?.walletAddress && nfts && nfts.length === 0) {
+      console.log(nfts)
+      console.log(nfts)
+      const response = await fetch(`/api/nfts/${session.data.walletAddress}`, {
+        method: "GET",
+      });
+
+      if (response.status == 200) {
+        const result = await response.json()
+        console.log(result)
+        setNFTs(result['ownedNfts'])
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (nftBalance >= 0) {
+      getNFTs()
+    }
+  }, [nftBalance])
 
   useEffect(() => {
     console.log('useeffect')
@@ -16,8 +39,6 @@ const ConnectedView = () => {
       if (session.data?.walletAddress) {
         const result: any = await getBalance(session);
         if (result) {
-          console.log('balance')
-          console.log(parseFloat(result.toString()))
           setNFTBalance(parseFloat(result.toString()))
         }
       }
@@ -37,15 +58,34 @@ const ConnectedView = () => {
         >
           {`Disconnect wallet`}
         </button>
-        <p>Current balance: {nftBalance}</p>
+        {nftBalance >= 0 && <p>Current balance: {nftBalance}</p>}
         <button
           className="bg-green-600 py-2 px-4 rounded-full text-white"
           onClick={async () => await mint()}
         >
           Mint
         </button>
-        <p></p>
       </div>
+      {nfts &&
+        <div className="grid md:grid-cols-4 md:gap-4 lg:grid-cols-8 lg:gap-8 grid-cols-2  gap-2">
+          {nfts.map((nft) => {
+            let image_link = nft['metadata']['image'] as string;
+            try {
+              if (image_link.startsWith('ipfs')) {
+                const split = image_link.split('/')
+                image_link = `https://ipfs.io/ipfs/${split[2]}/${split[3]}`
+              }
+              return <div>
+                <img width={500} height={500} src={image_link} />
+              </div>
+            } catch (error) {
+              console.log('error')
+              console.log(error)
+              return;
+            }
+          })}
+        </div>}
+
     </>
   );
 };
