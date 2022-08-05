615517b37cf9819356c85565d9828183c5386ef6
Rahul Patni <rahul20patni@gmail.com>
2022-08-05 13:23:08 -0700

    Adding deployment and verification

---
+++ smartcontracts/scripts/deployTestnet.ts
@@ -0,0 +1,37 @@
+import { writeFileSync } from "fs";
+import { ethers } from "hardhat";
+
+async function main() {
+  const Cypherpunk = await ethers.getContractFactory("Cypherpunk");
+  const contract = await Cypherpunk.deploy(
+    "https://ikzttp.mypinata.cloud/ipfs/QmQFkLSQysj94s5GvTHPyzTxrawwtjgiiYS2TBLgrvw8CW/",
+    ethers.utils.parseEther("0.05")
+  );
+
+  await contract.deployed();
+
+  console.log("Cypherpunk deployed to:", contract.address);
+  writeContractAddressToFile("contract", contract.address);
+}
+
+function writeContractAddressToFile(filename: string, contractAddress: string) {
+  try {
+    writeFileSync(
+      `./${filename}.json`,
+      JSON.stringify({
+        contractAddress: contractAddress,
+      })
+    );
+  } catch (error) {
+    console.error(`Error writing to ${filename}`);
+    console.error(error);
+    return;
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
